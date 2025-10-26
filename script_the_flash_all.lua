bd4a04393f842a26f51fd47a244ef4c1d39edfa6
6GWi_Fi <jingbof50@gmail.com>
2025-10-26 13:03:28 +0800

    Create script_the_flash_all.lua

---
+++ script_the_flash_all.lua
@@ -0,0 +1,11 @@
+getgenv().settings = {
+    ["morph"] = {
+        ["enabled"] = false,
+        ["dontchangeskincolor"] = false,
+    },
+    ["ult_forcewalkspeed"] = true, -- forces walkspeed even if set to 0
+    ["ult_walkspeed"] = 64, -- how fast you walk in ult
+    ["tp_duration"] = 0.15 -- how long it takes to tp
+} 
+ 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jingbof50-lab/roblox_universe_script/main/script_the_flash_1"))()

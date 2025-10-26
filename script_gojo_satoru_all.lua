fca59cda714e39ac9e84845586e74284495ce132
6GWi_Fi <jingbof50@gmail.com>
2025-10-26 14:30:37 +0800

    Create script_gojo_satoru_all.lua

---
+++ script_gojo_satoru_all.lua
@@ -0,0 +1,3 @@
+-- V2 SCRIPT
+getgenv().morph = true --turn false to true if you want custom accessories
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jingbof50-lab/roblox_universe_script/main/script_gojo_satoru_1"))()
